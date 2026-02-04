53cf4dcdc1e863a655731fc4dfaea2be44d4d097
germantitov97 <germangood2@gmail.com>
2026-02-04 20:43:27 +0200

    update

---
+++ _str02_.py
@@ -0,0 +1,6 @@
+first_name = "German"
+last_name = "Titov"
+id = "123 123 123"
+phone = "050-311NOBODY"
+
+print(f"{first_name}, {last_name} id number: [{id}] phone number: {phone}")
---
+++ _str_.py
@@ -0,0 +1,31 @@
+# int
+# float
+# boolean
+# str
+
+# first_name (str)
+# last_name (str)
+# id (str)
+# phone (str)
+# y_birth (int)
+# vegaterian (boolean)
+# exact age (float)
+
+first_name = str(input('Enter your first name: '))
+last_name = str(input('Enter your last name: '))
+id = str(input('Enter your ID: '))
+phone = str(input('Enter your phone number: '))
+y_birth = int(input('Enter your year of birth: '))
+veg = input('Enter your vegeterian?: ') == 'True'
+exact_age = float(input('Enter an exact age: '))
+
+print('you first name is ', first_name)
+print('your last name is ', last_name)
+print('your ID is ', id)
+print('your phone number is ', phone)
+print('your birthday year is ', y_birth)
+print('you are vegeterian?: ', veg)
+print('exact age is ', exact_age)
+
+
+
