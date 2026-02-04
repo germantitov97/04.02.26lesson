0a80e4f3667c303b26cf325f683427e4c055b54c
germantitov97 <germangood2@gmail.com>
2026-02-04 20:53:32 +0200

    update

--- _str02_.py
+++ _str02_.py
@@ -4,3 +4,10 @@ id = "123 123 123"
 phone = "050-311NOBODY"
 
 print(f"{first_name}, {last_name} id number: [{id}] phone number: {phone}")
+
+print(len(phone))
+
+# outer execution
+print(str.upper(first_name))
+# inner execution
+print(first_name.upper())
